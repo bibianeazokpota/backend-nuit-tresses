32cdefcad6fdb51f0e95f28053b6d325f43fa788
bibianeazokpota <bayibibianeazokpota@gmail.com>
2025-06-02 11:58:22 +0100

    Mise à jour cloudinary.js et ajout de node-fetch

--- cloudinary.js
+++ cloudinary.js
@@ -2,7 +2,7 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "divuihocg", // Ton nom de nuage Cloudinary
+  cloud_name: "divuihocg", 
   api_key: "451918873861155",
   api_secret: "Kj5jpeBD5OC7e8sNq_S26tJqswE",
 });
